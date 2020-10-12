0224f6f01f45a0f67d4fac7c6d6feb132f96c3d0
Louis Paret <louis.paret@gmail.com>
2020-10-12 23:25:04 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kubernetes-cluster
 Code of the testing Kubernetes cluster 
+
+Use Terraform v0.13.4
+
+source .env && terraform init
+source .env && terraform apply
+export KUBECONFIG=$PWD/kubeconfig
+kubectl get node -o wide
---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "scaleway" {}
+
+resource "scaleway_k8s_cluster_beta" "testing" {
+  name = "test-myfingerprint"
+  description = "testing cluster"
+  version = "1.19.2"
+  cni = "cilium"
+#   enable_dashboard = true
+#   ingress = "nginx"
+}
+
+resource "scaleway_k8s_pool_beta" "testing" {
+  cluster_id = scaleway_k8s_cluster_beta.testing.id
+  name = "testing"
+  node_type = "DEV1-M"
+  size = 1
+  autohealing = true
+}
+
+resource "local_file" "kubeconfig" {
+  content = scaleway_k8s_cluster_beta.testing.kubeconfig[0].config_file
+  filename = "${path.module}/kubeconfig"
+}
+
+output "cluster_url" {
+  value = scaleway_k8s_cluster_beta.testing.apiserver_url
+}
