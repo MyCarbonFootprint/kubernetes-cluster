7e24bcb3efb2a6f67d8680fb6dcba731f40f72f2
Louis Paret <louis.paret@gmail.com>
2020-11-02 23:03:46 +0100

    Add host and token in output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,13 @@ output "prometheus_url" {
 output "alertmanager_url" {
     value = "alertmanager${replace(module.kube_cluster.cluster_wildcard_dns, "*", "")}"
 }
+
+output "kube_host" {
+    value = module.kube_cluster.kubeconfig[0].host
+    sensitive   = true
+}
+
+output "kube_token" {
+    value = module.kube_cluster.kubeconfig[0].token
+    sensitive   = true
+}
